beb4110742cb3ec9ac332341ecb07b9c49fd47ef
manonab <45658243+manonab@users.noreply.github.com>
2022-01-25 17:28:47 +0100

    almost finished the contact form, need to add some design but it is working. Email link + app

--- manon-website/server/index.js
+++ manon-website/server/index.js
@@ -1,17 +1,21 @@
 require('./config/db');
 require('dotenv').config();
+const cors = require('cors');
 
 const http = require('http');
 const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const moment = require('moment');
+
 const app = express();
-const server = http.createServer(app);
-const cors = require('cors');
-const PORT = process.env.PORT || 3000
 app.use(express.json());
 app.use(cors());
+app.use(express.urlencoded({extended:true}))
+
+const PORT = process.env.PORT || 4000
+//nodemailer part
+const nodemailer = require('nodemailer');
+
+
+
 
 app.get('/', cors(), async(req,res) => {
     res.send('this is working')
@@ -20,3 +24,30 @@ app.get('/', cors(), async(req,res) => {
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`)
 });
+
+app.post('/post_name', async (req, res) => {
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'abelcoindozm@gmail.com',
+            pass:'mikyyjiyacfidlaf'
+        }
+    });
+
+    let {name, company, content, email } = req.body;
+    
+    let details = {
+        subject:`Message de ${name} et qui bosse à ${company}`,
+        text:`${content + " ." + " Adresse pour les contacter : " + email}`,
+        to:`abelcoindozm@gmail.com`,
+        from:`${email}`
+    }
+    transporter.sendMail(details, (err) => {
+        if(err){
+            res.send(err)
+        }else{
+            res.send('message sent !')
+        }
+    })
+
+})
