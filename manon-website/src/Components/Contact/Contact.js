05686a1963c244e15893083b28b8d5cda9b90afc
manonab <abelcoindozm@gmail.com>
2022-02-15 19:06:00 +0100

    added the form

--- manon-website/src/Components/Contact/Contact.js
+++ manon-website/src/Components/Contact/Contact.js
@@ -23,56 +23,102 @@ const Contact = ({ switchLanguage }) => {
         .catch((err) => console.log(err))
         };
     return (
-        <div>
-            <div className='bigTitle'>
-                <span class='cut-contact'>Contact</span>
-            </div>
-            {modal ?  
-            <div>
-                <h1>Message envoyé !</h1>
-            </div>
-            :null}
-            {switchLanguage ?
-                <>
-                    <p className='paragraph'>Vous pouvez me contacter via <FaLinkedinIn color='gold' />, ou via l'adresse suivante: <b style={{ color: 'gold' }}>abelcoindozm@gmail.com</b> ou alors directement via ce formulaire.</p>
-                    <form className='form' onSubmit={(e) => sendForm(e)}>
-                        <h4 className='titleForm'>Votre nom</h4>
-                        <input value={name} required placeholder='nom' onChange={(e) => setName(e.target.value)}></input>
-                        <h4 className='titleForm'>Votre entreprise</h4>
-                        <input value={company} required placeholder='entreprise' onChange={(e) => setCompany(e.target.value)}></input>
-                        <h4 className='titleForm'>Votre message</h4>
-                        <textarea required placeholder='message' onChange={(e) => setContent(e.target.value)}></textarea>
-                        <h4 className='titleForm'>Où puis-je vous contacter ?</h4>
-                        <input value={email} required placeholder='adresse email' onChange={(e) => setEmail(e.target.value)}></input>
-                        <button type="submit">ENVOYER</button>
-                    </form>
-                </>
-                :
-                <>
-                    <p className='paragraph'>You can reach me out via
-                        <span className='linkedin'>
-                            <a
-                                className='alink'
-                                href='https://www.linkedin.com/in/manon-abel-coindoz-b6449211a/'
-                                target={'_blank'}>
-                                <FaLinkedinIn size={30} style={{marginLeft:"05px", marginRight:'05px'}} />
-                            </a>
-                        </span>, or here: <b style={{ color: 'gold' }}>abelcoindozm@gmail.com</b> or directly via this form.</p>
-                    <form className='form' onSubmit={(e) => sendForm(e)}>
-                        <h4 className='titleForm'>Your name</h4>
-                        <input value={name} required placeholder='name' onChange={(e) => setName(e.target.value)}></input>
-                        <h4 className='titleForm'>Your company</h4>
-                        <input value={company} required placeholder='company' onChange={(e) => setCompany(e.target.value)}></input>
-                        <h4 className='titleForm'>Your message</h4>
-                        <textarea required placeholder='message' onChange={(e) => setContent(e.target.value)}></textarea>
-                        <h4 className='titleForm'>Where can I reach you out ?</h4>
-                        <input value={email} required placeholder='email adress' onChange={(e) => setEmail(e.target.value)}></input>
-                        <button type="submit">SEND</button>
-                    </form>
-                </>
-            }
+      <div>
+        <div className="bigTitle">
+          <span class="cut-contact">Contact</span>
         </div>
-    )
+        {switchLanguage ? (
+          <>
+            <p className="paragraph">
+              Vous pouvez me contacter via <FaLinkedinIn color="gold" />, ou via
+              l'adresse suivante:{" "}
+              <b style={{ color: "gold" }}>abelcoindozm@gmail.com</b> ou alors
+              directement via ce formulaire.
+            </p>
+            <form className="form" onSubmit={(e) => sendForm(e)}>
+              <h4 className="titleForm">Votre nom</h4>
+              <input
+                value={name}
+                required
+                placeholder="nom"
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+              <h4 className="titleForm">Votre entreprise</h4>
+              <input
+                value={company}
+                required
+                placeholder="entreprise"
+                onChange={(e) => setCompany(e.target.value)}
+              ></input>
+              <h4 className="titleForm">Votre message</h4>
+              <textarea
+                required
+                placeholder="message"
+                onChange={(e) => setContent(e.target.value)}
+              ></textarea>
+              <h4 className="titleForm">Où puis-je vous contacter ?</h4>
+              <input
+                value={email}
+                required
+                placeholder="adresse email"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <button type="submit">ENVOYER</button>
+            </form>
+          </>
+        ) : (
+          <>
+            <p className="paragraph">
+              You can reach me out via
+              <span className="linkedin">
+                <a
+                  className="alink"
+                  href="https://www.linkedin.com/in/manon-abel-coindoz-b6449211a/"
+                  target={"_blank"}
+                >
+                  <FaLinkedinIn
+                    size={30}
+                    style={{ marginLeft: "05px", marginRight: "05px" }}
+                  />
+                </a>
+              </span>
+              , or here: <b style={{ color: "gold" }}>abelcoindozm@gmail.com</b>{" "}
+              or directly via this form.
+            </p>
+            <form className="form" onSubmit={(e) => sendForm(e)}>
+              <h4 className="titleForm">Your name</h4>
+              <input
+                value={name}
+                required
+                placeholder="name"
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+              <h4 className="titleForm">Your company</h4>
+              <input
+                value={company}
+                required
+                placeholder="company"
+                onChange={(e) => setCompany(e.target.value)}
+              ></input>
+              <h4 className="titleForm">Your message</h4>
+              <textarea
+                required
+                placeholder="message"
+                onChange={(e) => setContent(e.target.value)}
+              ></textarea>
+              <h4 className="titleForm">Where can I reach you out ?</h4>
+              <input
+                value={email}
+                required
+                placeholder="email adress"
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <button type="submit">SEND</button>
+            </form>
+          </>
+        )}
+      </div>
+    );
 }
 
 export default Contact
--- manon-website/src/Components/Home/Develop.js
+++ manon-website/src/Components/Home/Develop.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { RiArrowDropRightFill } from "react-icons/ri";
-import { FaGithub,FaLinkedinIn,FaGooglePlusG } from "react-icons/fa";
+import { FaGithub, FaLinkedinIn, FaGooglePlusG } from "react-icons/fa";
 import { AiOutlineForm } from "react-icons/ai";
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import { MdEmail } from "react-icons/md";
 
-const Develop = () => {
+const Develop = ({ switchLanguage }) => {
+
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [content, setContent] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [modal, setModal] = useState(false);
+
+  const sendForm = async () => {
+    console.log(name, company, content, email)
+    let post = await axios
+      .post("https://manonwebsite.herokuapp.com/post_name", {
+        name: name,
+        company: company,
+        content: content,
+        email: email,
+      })
+      .then(() => console.log('message sent'))
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <>
+    <div className="goupe">
       <div className="testHome">
         <h1 className="bigTitle">Développer</h1>
         <div className="arrow">
@@ -19,12 +41,14 @@ const Develop = () => {
           />
         </div>
         <div className="textEnd">
-          <p className="textDEv"> 
+          <p className="textDEv">
             Et si maintenant on s'appelait ? Si mon univers t'as parlé,
             n'hésites pas à me contacter par mail (abelcoindozm@gmail.com) ou à
             me laisser un message sur LinkedIn.
           </p>
-          <p className="textDEv">Tu peux découvrir mon compte GitHub pour toujours plus de code.</p>
+          <p className="textDEv">
+            Tu peux découvrir mon compte GitHub pour toujours plus de code.
+          </p>
         </div>
         <ul className="iconGroup">
           <li>
@@ -37,7 +61,11 @@ const Develop = () => {
             </a>
           </li>
           <li>
-            <a className="icon" href="https://github.com/manonab"target={"_blank"}>
+            <a
+              className="icon"
+              href="https://github.com/manonab"
+              target={"_blank"}
+            >
               <FaGithub size={43} color={"#973465"} />
             </a>
           </li>
@@ -57,8 +85,38 @@ const Develop = () => {
           </li>
         </ul>
       </div>
-    </>
+      <form className="formulaire" onSubmit={(e) => sendForm(e)}>
+        <h4>Votre nom</h4>
+        <input
+          value={name}
+          required
+          placeholder="nom"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <h4>Votre entreprise</h4>
+        <input
+          value={company}
+          required
+          placeholder="entreprise"
+          onChange={(e) => setCompany(e.target.value)}
+        ></input>
+        <h4>Votre message</h4>
+        <textarea
+          required
+          placeholder="message"
+          onChange={(e) => setContent(e.target.value)}
+        ></textarea>
+        <h4>Où puis-je vous contacter ?</h4>
+        <input
+          value={email}
+          required
+          placeholder="adresse email"
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <button className="button" type="submit">ENVOYER</button>
+      </form>
+    </div>
   );
-}
+};
 
 export default Develop;
